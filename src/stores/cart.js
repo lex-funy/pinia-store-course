ed85210a418bcf0ce4ed11af4ff5ec77a437ee1f
Lex Krooswijk <lexkrooswijk@live.nl>
2023-09-11 13:47:04 +0200

    add computed cartTotal to store

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,10 +1,16 @@
 import { defineStore } from "pinia"
-import { ref } from "vue"
+import { computed, ref } from "vue"
 
 // use[storeId]Store
 // Setup store always returns an object
 export const useCartStore = defineStore('cart', () => {
     const cart = ref([])
 
-    return { cart }
+    // Because this is a computed property, it will be updated when the cart changes
+    const cartTotal = computed(() => cart.value.reduce((prev, cur) => prev += cur.price, 0))
+
+    return { 
+        cart, 
+        cartTotal,
+    }
 })
