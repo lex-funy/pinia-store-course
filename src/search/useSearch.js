2a80a0f71cba98072d461b1ef871e2875496afcc
Lex Krooswijk <lexkrooswijk@live.nl>
2023-09-13 11:48:55 +0200

    add product store to search

--- src/search/useSearch.js
+++ src/search/useSearch.js
@@ -1,15 +1,16 @@
 import { computed, onMounted } from 'vue'
-import products from '@/catalog/product-data'
-
+import { useProductStore } from '../stores/product'
 
 export default function useSearch(searchTerm) {
+  // Like cart.js:5
+  const productStore = useProductStore()
 
   const results = computed(() => {
     let searchResults
-    if (!searchTerm.value) searchResults = products
+    if (!searchTerm.value) searchResults = productStore.products
     else {
       const lowerTerm = searchTerm.value.toLowerCase()
-      searchResults = products.filter(
+      searchResults = productStore.products.filter(
         (product) => product.name.toLowerCase().includes(lowerTerm),
       )
     }
--- src/stores/cart.js
+++ src/stores/cart.js
@@ -2,11 +2,16 @@ import { defineStore, storeToRefs } from "pinia"
 import { computed, ref } from "vue"
 import { useProductStore } from "./product"
 
+// This should ALWAYS be called inside of de defineStore function. 
+// Otherwise Pinia may not be loaded yet.
+// `const { products } = storeToRefs(useProductStore())`
+
 // use[storeId]Store
 // Setup store always returns an object
 export const useCartStore = defineStore('cart', () => {
     // Use the product store to manage the products
     // Only save the product ids in the cart
+    // See :5
     const { products } = storeToRefs(useProductStore())
     const productIds = ref([])
 
